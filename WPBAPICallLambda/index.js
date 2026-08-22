1432f7ce7d5e365c915f856ad61517d261379a98
Aro Marutyan <aro.maru7ian@gmail.com>
2026-08-22 22:36:52 +0200

    added getObj to policy

--- WPBAPICallLambda/index.js
+++ WPBAPICallLambda/index.js
@@ -21,7 +21,7 @@ export const handler = async () => {
 
 async function handleResults(search, results) {
   if (!results.length) return;
-  console.log('TEST WORKFLOW API CALL - EU WEST my ass!');
+  console.log('TEST WORKFLOW API CALL - get object');
 
   const newestResults = getNewestResults(results, search?.newestOffer?.offerId, search?.newestOffer?.modified);
   
